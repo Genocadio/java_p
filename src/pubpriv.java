3245f520d9bcded15ba1351369ab3a652151d336
Geno <yvesgeno@outlook.com>
2023-08-16 16:12:46 +0200

    first program

---
+++ src/pubpriv.java
@@ -0,0 +1,10 @@
+public class pubpriv {
+    public static void main(String[] args) {
+        System.out.println("hello geno");
+        myname();
+
+    }
+    public static void myname(){
+        System.out.println("my names are :Geno Yves Cadiot");
+    }
+}
